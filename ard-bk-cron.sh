2976b100c8cfc85ecc2cdfcf5d2f7ddbf58674ca
Frederick Henderson <frederickjh@henderson-meier.org>
2016-02-17 09:55:40 +0100

    Added cron script and configuration file.

---
+++ ard-bk-cron.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Drush archive-dump backup cron
+# This script is used to make a archive-dump of the files and database for a
+# Drupal Website. It is intended to be scheduled to run by cron.
